0c452a64519bb0eebeeec3e7cd33690a2585ba46
Lance Johnson <rlancejohnson@gmail.com>
2022-01-25 15:21:36 -0700

    Implement loading bar and login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { handleSetInitialData } from './actions/shared'
 
+import LoadingBar from 'react-redux-loading-bar'
 import Header from './components/Header'
 
 import Login from './pages/Login'
@@ -14,24 +15,30 @@ import Leaderboard from './pages/Leaderboard'
 
 export default function App() {
     const dispatch = useDispatch()
-    const authedUser = useSelector((state) => state.authedUser)
+    const { authedUser, loading } = useSelector((state) => {
+        return {
+            authedUser: state.authedUser,
+            loading: state.loadingBar.default !== 0
+        }
+    })
 
     useEffect(() => {
         dispatch(handleSetInitialData())
-    })
+    }, [dispatch])
 
     return (
         <BrowserRouter>
             <div>
                 <Header />
+                <LoadingBar />
 
-                {authedUser === null &&
+                {(authedUser === null && loading === false) &&
                     <Routes>
                         <Route path='/' element={<Login />} />
                     </Routes>
                 }
 
-                {authedUser !== null &&
+                {(authedUser !== null && loading === false) &&
                     <Routes>
                         <Route path='/' element={<Home />} />
                         <Route path='/question/:id' element={<QuestionDetail />} />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import styles from './Login.module.css'
+import logo from '../assets/images/logo_large.png'
+
 export default function Login() {
+    const dispatch = useDispatch()
+    const users = useSelector((state) => state.users)
+
+    console.log(users)
+
     return (
-        <div>
+        <div className={styles['layout']}>
+            <div className={styles['section']}>
+                <img
+                    src={logo}
+                    alt='main logo'
+                    className={styles['logo']}
+                />
+            </div>
+            <div className={styles['section']}>
+                <select className={styles['user-selector']}>
+                    {Object.keys(users).map((userId) => (
+                        <option
+                            key={userId}
+                            value={userId}>
+                            {users[userId].name}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
         </div>
     )

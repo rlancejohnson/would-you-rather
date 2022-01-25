3cf81b09dd8b122fe3e3d10dfc80e615e8b82c5d
Lance Johnson <rlancejohnson@gmail.com>
2022-01-25 00:08:07 -0700

    Implement avatar cmp

---
+++ src/components/Avatar.js
@@ -0,0 +1,19 @@
+import styles from './Avatar.module.css'
+
+export default function Avatar({ url, diameter }) {
+    const size = {
+        width: diameter,
+        height: diameter
+    }
+
+    return (
+        <div>
+            <img
+                src={url}
+                alt='avatar'
+                className={styles['avatar-image']}
+                style={size}
+            />
+        </div>
+    )
+}

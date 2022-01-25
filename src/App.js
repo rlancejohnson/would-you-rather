8dc895c8e613d767cc354a25845d27364052130e
Lance Johnson <rlancejohnson@gmail.com>
2022-01-25 13:27:28 -0700

    Add pages to app cmp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import { BrowserRouter } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { handleSetInitialData } from './actions/shared'
 
 import Header from './components/Header'
 
+import Login from './pages/Login'
+import Home from './pages/Home'
+import QuestionDetail from './pages/QuestionDetail'
+import CreateQuestion from './pages/CreateQuestion'
+import Leaderboard from './pages/Leaderboard'
+
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-      </div>
-    </BrowserRouter>
-  )
+    const dispatch = useDispatch()
+    const authedUser = useSelector((state) => state.authedUser)
+
+    useEffect(() => {
+        dispatch(handleSetInitialData())
+    })
+
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+
+                {authedUser === null &&
+                    <Routes>
+                        <Route path='/' element={<Login />} />
+                    </Routes>
+                }
+
+                {authedUser !== null &&
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/question/:id' element={<QuestionDetail />} />
+                        <Route path='/add' element={<CreateQuestion />} />
+                        <Route path='/leaderboard' element={<Leaderboard />} />
+                    </Routes>
+                }
+            </div>
+        </BrowserRouter>
+    )
 }
---
+++ src/pages/CreateQuestion.js
@@ -0,0 +1,7 @@
+export default function CreateQuestion() {
+    return (
+        <div>
+            
+        </div>
+    )
+}

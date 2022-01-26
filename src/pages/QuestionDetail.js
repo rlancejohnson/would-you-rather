129c9a1b51feba21912cfae28001c7324d8d40b9
Lance Johnson <rlancejohnson@gmail.com>
2022-01-26 02:19:45 -0700

    Add stats badge after question is answered

--- src/pages/QuestionDetail.js
+++ src/pages/QuestionDetail.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { handleAddAnswer } from '../actions/questions'
 import { getClasses } from '../services/utils'
@@ -10,12 +10,21 @@ import Avatar from '../components/Avatar'
 
 export default function QuestionDetail() {
     const { id } = useParams()
+    const navigate = useNavigate()
     const [selectedOption, setSelectedOption] = useState(null)
     const dispatch = useDispatch()
     const { question, users } = useSelector(state => {
         return {
-            question: state['questions'][id],
+            question: id in state['questions'] ? state['questions'][id] : '',
             users: state.users
+        }
+    })
+
+    useEffect(() => {
+        if (!question) {
+            setTimeout(() => {
+                navigate('/')
+            }, 3000)
         }
     })
 
@@ -25,6 +34,15 @@ export default function QuestionDetail() {
             setSelectedOption(answer)
             dispatch(handleAddAnswer(question.id, answer))
         }
+    }
+
+    if (!question) {
+        return (
+            <div className={getClasses(styles, ['grid-vertical', 'grid-gap-small', 'content-area', 'title'])}>
+                <div>We were unable to find your question in our filing cabinet.</div>
+                <div>Please try selecting another question.</div>
+            </div>
+        )
     }
 
     return (
@@ -41,20 +59,36 @@ export default function QuestionDetail() {
                     Would you rather...
                 </div>
                 <div className={getClasses(styles, ['grid', 'grid-gap-large'])}>
-                    <div
-                        id='optionOne'
-                        className={getClasses(styles, selectedOption && selectedOption === 'optionOne' ? ['option-btn-selected'] : selectedOption ? ['option-btn-disabled'] : ['option-btn'])}
-                        onClick={handleSelection}>
-                        {question.optionOne.text}
+                    <div className={getClasses(styles, ['grid-vertical'])}>
+                        <div
+                            id='optionOne'
+                            className={getClasses(styles, selectedOption && selectedOption === 'optionOne' ? ['option-btn-selected'] : selectedOption ? ['option-btn-disabled'] : ['option-btn'])}
+                            onClick={handleSelection}>
+                            {question.optionOne.text}
+                        </div>
+                        {selectedOption &&
+                            <div className={getClasses(styles, ['stats-badge', 'grid-vertical'])}>
+                                <div>{`${question.optionOne.votes.length} Friends Chose`}</div>
+                                <div>{`${parseInt((question.optionOne.votes.length / Object.keys(users).length) * 100)}%`}</div>
+                            </div>
+                        }
                     </div>
                     <div className={getClasses(styles, ['template-text'])}>
                         OR
                     </div>
-                    <div
-                        id='optionTwo'
-                        className={getClasses(styles, selectedOption && selectedOption === 'optionTwo' ? ['option-btn-selected'] : selectedOption ? ['option-btn-disabled'] : ['option-btn'])}
-                        onClick={handleSelection}>
-                        {question.optionTwo.text}
+                    <div className={getClasses(styles, ['grid-vertical'])}>
+                        <div
+                            id='optionTwo'
+                            className={getClasses(styles, selectedOption && selectedOption === 'optionTwo' ? ['option-btn-selected'] : selectedOption ? ['option-btn-disabled'] : ['option-btn'])}
+                            onClick={handleSelection}>
+                            {question.optionTwo.text}
+                        </div>
+                        {selectedOption &&
+                            <div className={getClasses(styles, ['stats-badge', 'grid-vertical'])}>
+                                <div>{`${question.optionTwo.votes.length} Friends Chose`}</div>
+                                <div>{`${parseInt((question.optionTwo.votes.length / Object.keys(users).length) * 100)}%`}</div>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>

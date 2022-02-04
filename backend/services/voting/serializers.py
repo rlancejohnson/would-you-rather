60b9f61f94f2fd3546462b1af8778dd54c44806f
Lance Johnson <rlancejohnson@gmail.com>
2022-02-04 13:07:14 -0700

    Add only vote one per user and question validation

--- backend/services/voting/serializers.py
+++ backend/services/voting/serializers.py
@@ -34,16 +34,20 @@ class VoteSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         question = validated_data['question']
         question_options = question.options.values()
-        option = validated_data['choice']
+        choice = validated_data['choice']
+        vote = Vote.objects.filter(user=user, question=question)
 
-        if option.label in [q_option['label'] for q_option in question_options]:
+        if len(vote) == 0 and choice.label in [q_option['label'] for q_option in question_options]:
             new_vote = Vote.objects.create(
                 user = user,
                 question = question,
-                choice = option
+                choice = choice
             )
 
-        else:
+        elif len(vote) == 0:
             raise ValueError('The choice must match one of the options related to the question.')
+
+        else:
+            raise ValueError('Only one vote can exist for each user and question.')
 
         return new_vote

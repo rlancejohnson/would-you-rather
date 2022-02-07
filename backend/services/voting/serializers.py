802481eb7892f3ff91bafcca1592b4b75dce8b8a
Lance Johnson <rlancejohnson@gmail.com>
2022-02-07 15:15:54 -0700

    Convert question list and create apis to serializer logic

--- backend/services/voting/serializers.py
+++ backend/services/voting/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from .models import Option, Question, Vote
 
 
-class QuestionSerializer(serializers.ModelSerializer):
+class GetQuestionSerializer(serializers.ModelSerializer):
     author = serializers.SerializerMethodField()
     timestamp = serializers.SerializerMethodField()
     optionOne = serializers.SerializerMethodField()
@@ -38,6 +38,39 @@ class QuestionSerializer(serializers.ModelSerializer):
             'text': question.option_two.label,
             'votes': [vote.voter.username for vote in question.votes.all() if vote.choice == question.option_two]
         }
+
+class CreateQuestionSerializer(serializers.ModelSerializer):
+    optionOne = serializers.CharField(max_length = 500, source='option_one')
+    optionTwo = serializers.CharField(max_length = 500, source='option_two')
+
+    class Meta:
+        model = Question
+        fields = (
+            'id',
+            'optionOne',
+            'optionTwo'
+        )
+
+    def create(self, question):
+        user = self.context['request'].user
+
+        if question['option_one'] == question['option_two']:
+            raise ValueError('Option one and two cannot be the same.')
+
+        req_option_labels = [question['option_one'], question['option_two']]
+
+        options = {option.label: option for option in Option.objects.filter(label__in=req_option_labels)}
+        qy_option_labels = [option.label for option in options.values()]
+        option_labels_to_create = [label for label in req_option_labels if label not in qy_option_labels]
+
+        if len(option_labels_to_create) > 0:
+            for label in option_labels_to_create:
+                options[label] = Option.objects.create(label = label)
+
+        new_question = Question.objects.create(author = user, option_one = options[question['option_one']], option_two = options[question['option_two']])
+
+        return new_question
+
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/services/voting/views.py
+++ backend/services/voting/views.py
@@ -3,38 +3,20 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .models import Question, Option, Vote
-from .serializers import QuestionSerializer, VoteSerializer
+from .serializers import CreateQuestionSerializer, GetQuestionSerializer, VoteSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
 
-    '''
-        Takes a list of option labels
-    '''
-    def create(self, request, *args, **kwargs):
-        req_option_labels = request.data
+    def get_serializer_class(self):
+        print(self.request)
+        return CreateQuestionSerializer if self.request.method == 'POST' else GetQuestionSerializer
 
-        if len(req_option_labels) > 2 or len(req_option_labels) < 2:
-            raise ValueError('A question must have exactly 2 options.')
-
-        qy_options = list(Option.objects.filter(label__in=req_option_labels))
-        qy_option_labels = [option.label for option in qy_options]
-        option_labels_to_create = [label for label in req_option_labels if label not in qy_option_labels]
-
-        if len(option_labels_to_create) > 0:
-            options_to_create = []
-
-            for label in option_labels_to_create:
-                options_to_create.append(Option.objects.create(label=label))
-
-            qy_options.extend(options_to_create)
-
-        new_question = Question.objects.create(author=request.user)
-        new_question.options.set(qy_options)
-
-        return Response(QuestionSerializer(new_question).data)
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({'request': self.request})
+        return context
 
 
 class VoteViewSet(viewsets.ModelViewSet):

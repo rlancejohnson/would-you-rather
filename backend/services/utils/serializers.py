0248f9fbf08839f878ae16a7e98904a63989af2c
Lance Johnson <rlancejohnson@gmail.com>
2022-02-07 16:51:35 -0700

    List to dict serializer util

---
+++ backend/services/utils/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from collections import OrderedDict
+
+
+class ReturnDict(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        self.serializer = kwargs.pop('serializer')
+        super(ReturnDict, self).__init__(*args, **kwargs)
+
+class DictSerializer(serializers.ListSerializer):
+    """
+    Overrides default ListSerializer to return a dict with a custom field from
+    each item as the key. Makes it easier to normalize the data so that there
+    is minimal nesting. dict_key defaults to 'id' but can be overridden.
+    """
+    dict_key = 'id'
+
+    @property
+    def data(self):
+        """
+        Overriden to return a ReturnDict instead of a ReturnList.
+        """
+        ret = super(serializers.ListSerializer, self).data
+        return ReturnDict(ret, serializer=self)
+
+    def to_representation(self, data):
+        """
+        Converts the data from a list to a dictionary.
+        """
+        items = super(DictSerializer, self).to_representation(data)
+        return {item[self.dict_key]: item for item in items}

2e91e87db4f4017d1c07fdeed1d5b063f9b8b1a9
Lance Johnson <rlancejohnson@gmail.com>
2022-02-07 18:09:59 -0700

    Implement duplicate vote validation

--- backend/services/voting/models.py
+++ backend/services/voting/models.py
@@ -19,14 +19,14 @@ class Question(models.Model):
         return f'{self.id} - {self.author.username} | {self.option_one.label}, {self.option_two.label}'
 
     def clean(self):
-        question_options = [f'{question.option_one.id}{question.option_two.id}' for question in Question.objects.filter(
+        question_keys = [f'{question.option_one.id}{question.option_two.id}' for question in Question.objects.filter(
             Q(option_one = self.option_one, option_two = self.option_two) | Q(option_one = self.option_two, option_two = self.option_one)
         )]
 
         if self.option_one == self.option_two:
             raise ValidationError('The two options related to a question must be different.')
 
-        elif f'{self.option_one.id}{self.option_two.id}' in question_options or f'{self.option_two.id}{self.option_one.id}' in question_options:
+        elif f'{self.option_one.id}{self.option_two.id}' in question_keys or f'{self.option_two.id}{self.option_one.id}' in question_keys:
             raise ValidationError('This question already exists.')
 
 
@@ -34,6 +34,12 @@ class Vote(models.Model):
     voter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='votes', on_delete=models.CASCADE)
     question = models.ForeignKey(Question, related_name='votes', on_delete=models.CASCADE)
     choice = models.ForeignKey(Option, related_name='votes', on_delete=models.CASCADE)
+
+    def clean(self):
+        vote_keys = [f'{vote.voter.id}{vote.question.id}' for vote in Vote.objects.filter(voter = self.voter, question = self.question)]
+
+        if f'{self.voter.id}{self.question.id}' in vote_keys:
+            raise ValidationError('You have already answered this question.')
 
     def __str__(self):
         return f'{self.id} - {self.voter.username} | {self.choice.label}'

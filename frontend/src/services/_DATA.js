8e913dd210157b1f82355b9920a1ac8e9af5a184
Lance Johnson <rlancejohnson@gmail.com>
2022-02-08 12:20:55 -0700

    Update api service module

--- frontend/src/services/_DATA.js
+++ frontend/src/services/_DATA.js
@@ -1,3 +1,15 @@
+export function _register() {
+    return new Promise((resolve) => {
+
+    })
+}
+
+export function _login() {
+    return new Promise((resolve) => {
+
+    })
+}
+
 export function _getUsers() {
     return new Promise((resolve) => {
         fetch('http://localhost:8000/api/v1/users/')
@@ -12,16 +24,40 @@ export function _getQuestions() {
     })
 }
 
-export function _saveQuestion(optionOneText, optionTwoText) {
+export function _saveQuestion({ optionOneText, optionTwoText }) {
     return new Promise((resolve) => {
-        fetch('http://localhost:8000/api/v1/question/', { body: JSON.stringify({ optionOne: optionOneText, optionTwo: optionTwoText }) })
-            .then((response) => resolve(response.json()))
+        fetch('http://localhost:8000/api/v1/question/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                optionOne: optionOneText,
+                optionTwo: optionTwoText
+            })
+        }).then((response) => {
+            console.log(response)
+            return resolve(response.json())
+        })
     })
 }
 
 export function _saveQuestionAnswer({ qid, answer }) {
     return new Promise((resolve) => {
-        fetch('http://localhost:8000/api/v1/vote/', { body: JSON.stringify({ question: qid, choice: answer }) })
-            .then((response) => resolve(response.json()))
+        fetch('http://localhost:8000/api/v1/vote/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                question: qid,
+                choice: answer
+            })
+        }).then((response) => {
+            console.log(response)
+            return resolve(response.json())
+        })
     })
 }

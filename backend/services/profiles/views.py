ccf4646d1823d3df3f7588db04c7bcd6609f2cae
Lance Johnson <rlancejohnson@gmail.com>
2022-02-09 15:03:26 -0700

    Resolve broken tuple issue

--- backend/services/profiles/views.py
+++ backend/services/profiles/views.py
@@ -13,7 +13,7 @@ class CustomAuthToken(ObtainAuthToken):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'username': user.username

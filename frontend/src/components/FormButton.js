f8605209865e594026db82c6795201c4dae9df9c
Lance Johnson <rlancejohnson@gmail.com>
2022-02-10 21:25:50 -0700

    Allow for custom color

--- frontend/src/components/FormButton.js
+++ frontend/src/components/FormButton.js
@@ -1,9 +1,10 @@
 import styles from './FormButton.module.css'
 
-export default function FormButton({ label, disabledCondition, handleClick }) {
+export default function FormButton({ label, color, disabledCondition, handleClick }) {
     return (
         <button
             className={disabledCondition ? styles['btn-disabled'] : styles['btn']}
+            style={{ backgroundColor: color }}
             disabled={disabledCondition}
             onClick={handleClick}>
             {label}
